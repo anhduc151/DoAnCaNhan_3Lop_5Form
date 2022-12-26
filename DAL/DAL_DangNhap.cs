67d483e61d30716912a4dac13029330de78229d0
anhduc151 <leanhduc1510@gmail.com>
2022-12-26 20:30:25 +0700

    Project c#

---
+++ DAL/DAL_DangNhap.cs
@@ -0,0 +1,22 @@
+﻿using DTO;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DAL_DangNhap : KetNoi
+    {
+        public int kiemTraDangNhap(DTO_DNDangNhap a)
+        {
+            string sql = "checkDangnhap";
+            SqlCommand comm = new SqlCommand(sql, conn);
+            comm.CommandType = System.Data.CommandType.StoredProcedure;
+            comm.Parameters.AddWithValue("@matk", a.Matk);
+            comm.Parameters.AddWithValue("@pass", a.Pass);
+            int kq = -1;
+            conn.Open();
+            kq = (int)comm.ExecuteScalar();
+            conn.Close();
+            return kq;
+        }
+    }
+}
